bbfb76470d8f77be8a04ea942d1b5f644dc7c5ca
TheTechnologyLover-CatalystBrains <piyush@thetechnolover.com>
2019-11-05 11:05:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
     if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
+        
         return res.status(200).json({});
     }
 
