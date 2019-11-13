740389e4e530e521bcf1c2bcf7e5fb710e680e77
Piyush Bendale <thetechnolover7@gmail.com>
2019-11-13 22:16:52 +0530

    option

--- app.js
+++ app.js
@@ -25,18 +25,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+app.use((req, res, next) => { 
+    res.header("Access-Control-Allow-Origin", "*") 
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization" 
+    );
     if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
-
     next();
-});
+})
 
 
 /**
